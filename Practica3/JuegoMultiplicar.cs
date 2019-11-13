da4831ed71ec160d89eccab27a7dd7bab03d1a05
Sara <mublasa@gmail.com>
2019-11-13 19:39:35 +0100

    Fisrt Commit

---
+++ Practica3/JuegoMultiplicar.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Text;
+
+namespace Practica3
+{
+    class JuegoMultiplicar
+    {
+        private byte op1, op2, res, aciertos, fallos, nPreg, tpo;
+
+        #region Constructores
+        public JuegoMultiplicar()
+        {
+            Op1 = 0;
+            Op2 = 0;
+            Res = 0;
+            nPreg = 0;
+            Aciertos = 0;
+            Fallos = 0;
+            tpo = 0;
+        }
+
+        public JuegoMultiplicar (byte tiempo)
+        {
+            Tpo = tiempo;
+            Aciertos = 0;
+            Fallos = 0;
+        }
+        #endregion
+
+        #region Propiedades
+        public byte Op1
+        {
+            get => op1;
+            set
+            {
+                if (value > 0 && value < 10)
+                    op1 = value;
+            }
+        }
+
+        public byte Op2
+        {
+            get => op2;
+            set
+            {
+                if (value > 0 && value < 10)
+                    op2 = value;
+            }
+        }
+
+        public byte Res
+        {
+            get => res;
+            set => res = value;
+        }
+
+        public byte Aciertos
+        {
+            get => aciertos;
+            set
+            {
+                if (value >= 0)
+                    aciertos = value;
+            }
+        }
+
+        public byte Fallos
+        {
+            get => fallos;
+            set
+            {
+                if (value >= 0)
+                    fallos = value;
+            }
+        }
+
+        public byte NPreg
+        {
+            get => nPreg;
+            set
+            {
+                if (value > 0 && value < 11)
+                    nPreg = value;
+                else
+                {
+                    //PruebaJuego.imprimirError("\nERROR. Nº de preguntas no válido.\n");
+                    PruebaJuego.imprimirError("\nERROR. Number of questions out of range.\n");
+                    PruebaJuego.esperaCorta();
+                }
+            }
+        }
+
+        public byte Tpo
+        {
+            get => tpo;
+            set
+            {
+                if (value > 2 && value < 11)
+                    tpo = value;
+                else
+                {
+                    //PruebaJuego.imprimirError("\nERROR. Tiempo no válido.\n");
+                    PruebaJuego.imprimirError("\nERROR. Time out of range.\n");
+                    PruebaJuego.esperaCorta();
+                }
+            }
+        }
+        #endregion
+
+        #region Métodos
+        public bool comprobarResultado (byte n)
+        {
+            bool acierto = (n == Res);
+
+            if (acierto)
+            {
+                haAcertado();
+            }
+            else
+            {
+                haFallado(0);
+            }
+
+            return acierto;
+        }
+
+        public void generarOperandos ()
+        {
+            Random rnd = new Random();
+            int n1, n2;
+
+            n1 = rnd.Next(1, 10);
+            Op1 = Convert.ToByte(n1);
+            n2 = rnd.Next(1,10);
+            Op2 = Convert.ToByte(n2);
+            Res = Convert.ToByte(n1 * n2);
+
+            nPreg = Convert.ToByte(NPreg - 1);
+        }
+
+        public void reiniciarJuego()
+        {
+            Aciertos = 0;
+            Fallos = 0;
+        }
+
+        public string responderIntervalo()
+        {
+            ConsoleKeyInfo ck = default(ConsoleKeyInfo);
+            DateTime inicio = DateTime.Now;
+            TimeSpan diferencia;
+            StringBuilder s = new StringBuilder();
+
+            do
+            {
+                diferencia = DateTime.Now - inicio;
+
+                if (Console.KeyAvailable)
+                {
+                    ck = Console.ReadKey(true);
+
+                    if (ck.Key == ConsoleKey.Enter)
+                    {
+                        break;
+                    }
+                    else if (ck.Key == ConsoleKey.Backspace && s.ToString().Length > 0)
+                    {
+                        s.Remove(s.Length - 1, 1);
+                        Console.Write("\b \b");
+                    }
+                    else
+                    {
+                        s.Append(ck.KeyChar.ToString());
+                        Console.Write(ck.KeyChar.ToString());
+                    }
+                }
+            }while (diferencia.Seconds < Tpo && ck.Key != ConsoleKey.Enter);
+
+            if (diferencia.Seconds >= Tpo)
+            {
+                s = new StringBuilder("-1");
+            }
+
+            return s.ToString();
+        }
+
+        public void haAcertado()
+        {
+            //PruebaJuego.imprimirVerde("\nMuy bien! Respuesta correcta.\n");
+            PruebaJuego.imprimirVerde("\nVery good! Correct answer.\n");
+            Aciertos = Convert.ToByte(Aciertos + 1);
+        }
+        
+        public void haFallado(int n)
+        {
+            if (n == -1)
+            {
+                //PruebaJuego.imprimirError("\nSe agotó el tiempo. La respuesta era " + Res + "\n");
+                PruebaJuego.imprimirError("\nTime is up. The answer was " + Res + "\n");
+                Fallos = Convert.ToByte(Fallos + 1);
+            }
+            else
+            {
+                //PruebaJuego.imprimirError("\nRespuesta incorrecta. ");
+                PruebaJuego.imprimirError("\nWrong answer. ");
+                //Console.Write("La respuesta era: ");
+                Console.Write("The answer was: ");
+                PruebaJuego.imprimirError("" + Res + "\n");
+                Fallos = Convert.ToByte(Fallos + 1);
+            }
+        }
+
+        public void mostrarResultado()
+        {
+            //Console.WriteLine("\n------- Resultado -------");
+            //PruebaJuego.imprimirVerde("  Aciertos: ");
+            //Console.Write(Aciertos);
+            //PruebaJuego.imprimirError(" Fallos: ");
+            //Console.Write(Fallos + "\n");
+
+            Console.WriteLine("\n------- Results -------");
+            PruebaJuego.imprimirVerde("  Right: ");
+            Console.Write(Aciertos);
+            PruebaJuego.imprimirError(" Wrong: ");
+            Console.Write(Fallos + "\n");
+        }
+        #endregion
+    }
+}
---
+++ Practica3/PruebaJuego.cs
@@ -0,0 +1,244 @@
+﻿using System;
+
+namespace Practica3
+{
+    class PruebaJuego
+    {
+        private JuegoMultiplicar juego;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Multiplying Game";
+            PruebaJuego p = new PruebaJuego();
+            p.juego = new JuegoMultiplicar();
+            ConsoleKey ck;
+
+            do
+            {
+                p.menu();
+                ck = Console.ReadKey().Key;
+                p.seleccionOpcion(ck);                    
+            }
+            while (ck != ConsoleKey.Escape);
+        }
+
+        #region Menú
+        private void menu ()
+        {
+            Console.Clear();
+            //imprimirVerde("\nJuego de multiplicar.\n\n");
+            //imprimirVerde("1. ");
+            //Console.WriteLine("Establecer tiempo máximo para las respuestas");
+            //imprimirVerde("2. ");
+            //Console.WriteLine("Establecer el número de preguntas");
+            //imprimirVerde("3. ");
+            //Console.WriteLine("Jugar");
+            //imprimirVerde("Esc. ");
+            //Console.WriteLine("Salir\n");
+            //imprimirVerde("Opción: ");
+
+            imprimirVerde("\nMultiplying Game.\n\n");
+            imprimirVerde("1. ");
+            Console.WriteLine("Set maximum answer time");
+            imprimirVerde("2. ");
+            Console.WriteLine("Set number of questions");
+            imprimirVerde("3. ");
+            Console.WriteLine("Play");
+            imprimirVerde("Esc. ");
+            Console.WriteLine("Exit\n");
+            imprimirVerde("Option: ");
+        }
+
+        private void seleccionOpcion (ConsoleKey ck)
+        {
+            if (!ck.Equals(ConsoleKey.Escape))
+            {
+                switch (ck)
+                {
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+                        leerTiempoLimite();
+                        break;
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
+                        leerNumeroPreguntas();
+                        break;
+                    case ConsoleKey.D3:
+                    case ConsoleKey.NumPad3:
+                        jugar();
+                        break;
+                    default:
+                        //imprimirError("\n\nERROR. Opción no válida.\n");
+                        imprimirError("\n\nERROR. Non-existent option.\n");
+                        esperaCorta();
+                        break;
+                }
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+        #endregion
+
+        #region Imprimir
+        public static void imprimirVerde (string s)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write(s);
+            Console.ResetColor();
+        }
+
+        private void imprimirAzul(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write(s);
+            Console.ResetColor();
+        }
+
+        public static void imprimirError(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(s);
+            Console.ResetColor();
+        }
+        #endregion
+
+        #region Comprobación
+        private bool comprobarByte(String s)
+        {
+            byte n;
+            bool correcto = false;
+
+            try
+            {
+                n = Convert.ToByte(s);
+                correcto = true;
+            }
+            catch (FormatException)
+            {
+                //imprimirError("\nERROR. Valor no numérico.\n");
+                imprimirError("\nERROR. Not a number.\n");
+            }
+            catch (OverflowException)
+            {
+                //imprimirError("\nERROR. Valor demasiado extenso o negativo.\n");
+                imprimirError("\nERROR. Negative value or out of range.\n");
+            }
+
+            return correcto;
+        }
+        #endregion
+
+        #region Esperas
+        public static void esperaLarga()
+        {
+            System.Threading.Thread.Sleep(2000);
+        }
+
+        public static void esperaCorta()
+        {
+            System.Threading.Thread.Sleep(800);
+        }
+        #endregion
+
+        #region Métodos
+        private void leerTiempoLimite()
+        {
+            bool tiempoCorrecto = false;
+
+            while (!tiempoCorrecto || juego.Tpo == 0)
+            {
+                //imprimirAzul("\n\nIntroduzca el tiempo límite (3-10s): ");
+                imprimirAzul("\n\nSet time limit (3-10s): ");
+
+                string s = Console.ReadLine();
+
+                tiempoCorrecto = comprobarByte(s);
+
+                if (tiempoCorrecto)
+                    juego.Tpo = Convert.ToByte(s);
+            }
+        }
+
+        private void leerNumeroPreguntas()
+        {
+            bool nPregCorrecto = false;
+
+            while (!nPregCorrecto || juego.NPreg == 0)
+            {
+                //imprimirAzul("\n\nIntroduzca el nº de preguntas (1-10): ");
+                imprimirAzul("\n\nSet number of questions (1-10): ");
+
+
+                string s = Console.ReadLine();
+
+                nPregCorrecto = comprobarByte(s);
+
+                if (nPregCorrecto)
+                    juego.NPreg = Convert.ToByte(s);
+            }
+        }
+
+        private void jugar()
+        {
+            if (juego.Tpo == 0 || juego.NPreg == 0)
+            {
+                //imprimirError("\n\nERROR. Primero debe establecer un tiempo límite y un número de preguntas.");
+                imprimirError("\n\nERROR. Settings not stablished.");
+                esperaLarga();
+            }
+            else
+            {
+                Console.Clear();
+
+                while (juego.NPreg > 0)
+                {
+                    string s = "";
+                    byte respuesta = 0;
+
+                    juego.generarOperandos();
+                    Console.Write("\n" + juego.Op1 + " * " + juego.Op2 + " = ");
+                    
+                    s = juego.responderIntervalo();
+
+                    if (s.Equals("-1"))
+                    {
+                        juego.haFallado(-1);
+                    }
+                    else if (!s.Equals("") && comprobarByte(s))
+                    {
+                        respuesta = Convert.ToByte(s);
+                        juego.comprobarResultado(respuesta);
+                    }   
+                    else
+                    {
+                        juego.haFallado(0);
+                    }                    
+                }
+
+                juego.mostrarResultado();
+
+                //Console.WriteLine("\nPulse cualquier tecla para continuar.");
+                Console.WriteLine("\nPress any key to continue.");
+                Console.ReadKey();
+
+                juego.reiniciarJuego();
+            }
+        }
+        #endregion
+
+        #region Idioma
+        private void idioma (String s)
+        {
+            if (s.Equals("en"))
+            {
+
+            }
+            else if (s.Equals("es"))
+            {
+            }
+        }
+        #endregion
+    }
+}
